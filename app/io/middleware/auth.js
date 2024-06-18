4a0cc0088f4e3b12ca734341012c3f1f30090d65
haha58 <1461089222@qq.com>
2024-06-18 20:39:38 +0800

    update:add 优化

--- app/io/middleware/auth.js
+++ app/io/middleware/auth.js
@@ -2,10 +2,21 @@
 
 module.exports = () => {
   return async (ctx, next) => {
-    const { socket, logger } = ctx;
+    const { socket, logger, helper } = ctx;
+    const { id } = socket;
     const query = socket.handshake.query;
-    logger.info('query', query);
-    await next();
-    console.log('disconnect');
+    try {
+      logger.info('query', query, id);
+      socket.emit('id', helper.parseMsg('connect', {
+        type: 'connect',
+        message: '云构建服务连接成功',
+        id,
+      }));
+      await next();
+      console.log('disconnect');
+    } catch (error) {
+      logger.error('build error', error.message);
+    }
+
   };
 };
